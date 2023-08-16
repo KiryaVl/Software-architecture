9330cd78e273a1bb00fbcabf4c13cfae3f3db4eb
Kirya <rabbitnya@gmail.com>
2023-08-16 12:32:39 +0300

    HW2 upgrade

--- Lession2/src/Main.java
+++ Lession2/src/Main.java
@@ -1,39 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
     public static void main(String[] args) {
+        // Создание генератора сундуков с золотом
         ItemFabric goldGenerator = new GoldGenerator();
         goldGenerator.openReward();
 
+        // Создание генератора сундуков с драгоценными камнями
         ItemFabric gemGenerator = new GemGenerator();
         gemGenerator.openReward();
-    }
-}
-/*
-    // ItemFabric generator = new GoldGenerator();
-    // generator.openReward();
-    // generator = new GemGenerator();
-    // generator.openReward();
-    //System.out.println("Hello, World!");
 
-    Random rnd = ThreadLocalRandom.current();
-    List<ItemFabric> fabricList = new ArrayList<>();
+        // Создание генератора пустых сундуков
+        ItemFabric emptyChestGenerator = new EmptyChestGenerator();
+        emptyChestGenerator.openReward();
 
+        // Создание генератора случайных чисел для выбора типа награды
+        Random rnd = ThreadLocalRandom.current();
+
+        // Список генераторов наград
+        List<ItemFabric> fabricList = new ArrayList<>();
         fabricList.add(new GoldGenerator());
         fabricList.add(new GemGenerator());
+        fabricList.add(new EmptyChestGenerator()); // Добавляем генератор пустых сундуков
 
-        for(int i =0; i<20; i++)
-        {
-        int index = Math.abs(rnd.nextInt()%2)==0?0:1;
-        ItemFabric fabric = fabricList.get(index);
-        fabric.openReward();
+        // Цикл для создания и открытия наград в разных сундуках
+        for (int i = 0; i < 20; i++) {
+            // Выбор случайного индекса для выбора генератора награды
+            int index = rnd.nextInt(fabricList.size());
+            ItemFabric fabric = fabricList.get(index);
+
+            // Создание и открытие награды с использованием текущего генератора
+            fabric.openReward();
         }
-*/
-/*
-* abstract class ItemFabric
-*   openReward
-*   createItem
-* interface iGameItem
-*   open
-*gold
-*gem
-*
-* */
+    }
+}
